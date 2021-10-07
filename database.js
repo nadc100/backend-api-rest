48b968d59f03f64830fe64ff16b63beb6c0773d5
nadc100 <nadc100@gmail.com>
2021-10-07 18:54:28 -0500

    delete the database connections tring

--- database.js
+++ database.js
@@ -1,7 +1,1 @@
-import mongoose from 'mongoose'
-
-mongoose.connect('mongodb+srv://nadc100:Meshugah2022***@cluster0.mtmzz.mongodb.net/mobiliariain?retryWrites=true&w=majority')
-.then(db=>console.log('Database connected!'))
-.catch(error=>console.log(error))
-
-
+import mongoose from 'mongoose';

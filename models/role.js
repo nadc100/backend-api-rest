5ee887c932945d86468ef135ec07fcc852e2b2e2
nadc100 <nadc100@gmail.com>
2021-10-07 20:21:16 -0500

    roles added, user for auth added, and routes for auth and user also added

---
+++ models/role.js
@@ -0,0 +1,10 @@
+import {Schema, model} from 'mongoose';
+
+const roleSchema = new Schema(
+    {
+    	name: String
+    }, {
+    	versionkey: false
+    });
+
+export default model("Role", roleSchema);

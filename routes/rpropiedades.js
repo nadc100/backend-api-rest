4617485bde1d54812dd2f7768365257ec7bc4422
nadc100 <nadc100@gmail.com>
2021-10-11 19:33:59 -0500

    more changes iun the server to make it work

--- routes/rpropiedades.js
+++ routes/rpropiedades.js
@@ -1,18 +1,18 @@
 const { Router } = require('express');
 const router = Router();
 const mongoose = require('mongoose');
-
-
-router.get('/:id', async (req, res) => {
-    try{
-       const Propiedad= await Propiedad.findById(req.params.id).exec();
-       console.log(Propiedad)
-       res.send(Propiedad);
-    }catch(err){
-       return res.status(500).send({
-        message: err.message
+//const Propiedad = require('./models/mpropiedades');
+router.get('/', async (req, res) => {
+   try {
+      const Propiedad = await Propiedad.find();
+      console.log(Propiedad)
+      res.send(Propiedad);
+   } catch (err) {
+      return res.status(500).send({
+         message: err.message
       });
-    }} );
+   }
+});
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
-const mongoose = require('mongoose');
+
 const path = require('path');
-const rutaspropiedades = require('./rutas/rutaspropiedades');
+const rutaspropiedades = require('./routes/rpropiedades');
 const app = express();
-const conn = require('./db');
+
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
 app.use(morgan('dev'));
+
 app.use(cors());
+
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use(conn);
-
-//rutas
 app.use('/propiedades', rutaspropiedades);
 
-app.set('PORT', process.env.PORT || 3000 );
+
 
 //middleware
-app.listen(app.get('PORT'), () => {
+app.listen(port, () => {
     console.log(`Server started on port: ${app.get('PORT')}`);
 });
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('index');
 })
